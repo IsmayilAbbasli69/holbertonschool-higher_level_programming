b94e64f0acd37e11d4bfa8b161220c16e85e33e3
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2025-12-18 14:40:14 +0000

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square Class
+
+A Square Class
+
+"""
+
+
+class Square:
+
+    def __init__(self, size):
+        """this is init
+        Attributes:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
