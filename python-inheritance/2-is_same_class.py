575713e00670a46855304ef8157aa09d86c36878
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2025-12-23 17:16:23 +0400

    add

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,7 +3,16 @@
 
 
 def is_same_class(obj, a_class):
+    """"
+
+    THis is a function
+
+    """
+
     if isinstance(obj,a_class):
+        """
+        Also this is comment
+        """
         return True
     else:
         return False
