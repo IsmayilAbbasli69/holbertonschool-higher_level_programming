4ea891b2b1d7777b74e98e1257bd23c3b99ed2ce
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2025-12-24 18:13:14 +0400

    git

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Hi"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        Rectangle.integer_validator(self, name="size", value=size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return '[Square] {}/{}'.format(self.__size, self.__size)
