2e51bf318eb7eb19cddaad622d43028af158b3df
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2025-12-29 13:12:17 +0400

    checking

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,6 +6,8 @@ Docstring for python-input_output.0-read_file
 
 
 def read_file(filename=""):
-    with open(filename,encoding="utf-8") as file:
-        text = file.read()
-        print(text)
+    """
+    FILE
+    """
+    with open(filename,"r",encoding="utf-8") as file:
+        print(file.read(), end="")
