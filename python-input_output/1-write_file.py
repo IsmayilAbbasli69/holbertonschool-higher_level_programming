cf4cd37df27ed75e05c3bad90f5f77d399829d70
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2025-12-29 13:21:17 +0400

    last touch

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -11,6 +11,3 @@ def write_file(filename="", text=""):
     """
     with open(filename, "w", encoding="utf-8") as file:
         print(file.write(text), end="")
-
-    with open(filename, "r", encoding="utf-8") as file:
-        print(len(file.read()))
