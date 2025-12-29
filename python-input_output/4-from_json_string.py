dce10aca5bbbff20f41b812737439afd8e72a10b
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2025-12-29 21:16:02 +0400

    loads

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that returns JSON representation of an object"""
+
+
+import json
+
+
+def from_json_string(my_str):
+
+    """
+    function
+    """
+
+    return json.loads(my_str)
