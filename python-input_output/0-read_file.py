1cc3439979e1f86c1d00c7336a7fe44884cd7d64
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2025-12-29 13:10:32 +0400

    input

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,8 @@ Docstring for python-input_output.0-read_file
 
 """
 
-with open("open.txt",encoding="utf-8") as file:
-    text = file.read()
-    print(text)
+
+def read_file(filename=""):
+    with open(filename,encoding="utf-8") as file:
+        text = file.read()
+        print(text)
