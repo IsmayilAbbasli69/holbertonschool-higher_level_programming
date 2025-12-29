6dafd4d644e0c43e43f9ba298b1a9f9e8e882f52
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2025-12-29 13:19:12 +0400

    commit

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Docstring for python-input_output.0-read_file
+
+"""
+
+
+def read_file(filename="", text=""):
+    """
+    FILE
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        print(file.write(text), end="")
+
+    with open(filename, "r", encoding="utf-8") as file:
+        print(len(file.read()))
