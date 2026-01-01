57cb859cefe6f39c17e031baf04ba44db062a55e
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2026-01-01 15:41:44 +0400

    funal touch

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
 """
-Module
+Docstring for python-input_output.6-load_from_json_file
 """
 
 
 class Student:
+    """
+    Docstring for Student
+    """
+
     def __init__(self, first_name, last_name, age):
+        """
+        Docstring for __init__
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
-    
+
     def to_json(self):
+        """
+        Docstring for to_json
+        """
         return self.__dict__
