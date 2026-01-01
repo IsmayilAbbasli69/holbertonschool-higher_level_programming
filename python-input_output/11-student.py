e1fa0fec4eb19015c523a241f0c23c0bbf1e388d
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2026-01-01 15:53:43 +0400

    11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Docstring for python-input_output.6-load_from_json_file
+"""
+
+
+class Student:
+    """
+    Docstring for Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Docstring for __init__
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Docstring for to_json
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            res = {}
+            for a in attrs:
+                if a in self.__dict__:
+                    res[a] = self.__dict__[a]
+            return res
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Docstring for reload_from_json
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
