686499c3237c538d9c00238cc9838c20c5642fad
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2026-01-03 21:17:11 +0400

    paascal

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,18 +4,18 @@
 
 def pascal_triangle(n):
     """
-    Docstring for pascal_triangle
-    
-    :param n: Description
+    Returns a list of lists of integers representing
+    the Pascal's triangle of n.
     """
-
-
     if n <= 0:
         return []
+
     triangle = []
+
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
+
     return triangle
