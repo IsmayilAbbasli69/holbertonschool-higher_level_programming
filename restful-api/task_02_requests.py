0b14af43e3928b9566cb6e964767dfddaf7bf4a2
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2026-01-09 13:02:00 +0400

    API

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,6 +1,35 @@
 import requests
+import csv
 
-href= requests.get("https://jsonplaceholder.typicode.com/posts")
+url = requests.get("https://jsonplaceholder.typicode.com/posts")
 
-print(href.status_code)
-print(href.json)
+
+def fetch_and_print_posts():
+    status = url.status_code
+    json_data = url.json()
+    if status == 200:
+        print(f"Status Code: {status}")
+        for data in json_data:
+            print(data["title"])
+    else:
+        print(f"Failed to fetch posts. Status Code: {status}")
+
+def fetch_and_save_posts():
+    status = url.status_code
+    json_data = url.json()
+    data = []
+    if status == 200:
+        for post in json_data:
+            data.append({
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            })
+
+        with open("posts.csv", "w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(
+                file,
+                fieldnames=data[0].keys()
+            )
+            writer.writeheader()
+            writer.writerows(data)
