e40f2f865bc5b73bbbdbcfda8ac5f16af1cddd38
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2026-01-16 17:21:23 +0400

    full list

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- Create Bitch
-CREATE TABLE IF NOT EXISTS second_table(
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table VALUES (1, "JOHN", 10), (2, "ALEX", 3),  (3, "Bob", 14), (4, "George", 8);
 
+INSERT INTO second_table VALUES (1, "John", 10), (2, "Alex", 3),  (3, "Bob", 14), (4, "George", 8);
