50757a057b8d8522f6a7a147cccaffe8fcda4ede
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2026-01-20 22:29:14 +0400

    8-cities

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
---Subquery_city
-SELECT id,name FROM cities WHERE state_id = (SELECT id FROM states WHERE name="California") ORDER BY id DESC;
+-- Subquery_city
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name="California") ORDER BY id DESC;
